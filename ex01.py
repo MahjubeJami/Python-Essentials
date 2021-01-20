5d54eab06d73e5745362609bbc5b5c594975167a
MahjubeJami <77431021+MahjubeJami@users.noreply.github.com>
2021-01-20 15:57:34 -0800

    Add files via upload

---
+++ ex01.py
@@ -0,0 +1,10 @@
+# 1. Write a program that uses input to prompt a user for their name
+# and then greets them to the Comp 660 class.
+    # Console Output
+    #
+    # Enter your first name: John
+    # Welcome John to Comp 660!
+
+
+firstname = input("Enter your first name: ")
+print("Welcome" , firstname , "to Comp 660!")
