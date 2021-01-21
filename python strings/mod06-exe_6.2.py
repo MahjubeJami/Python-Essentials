48cceecf1bccdc0f18d2cc0fd2c879cf18dab5dc
MahjubeJami <77431021+MahjubeJami@users.noreply.github.com>
2021-01-21 00:17:24 -0800

    Update mod06-exe_6.2.py

--- python strings/mod06-exe_6.2.py
+++ python strings/mod06-exe_6.2.py
@@ -1,1 +1,14 @@
+"""
+Take the following Python
+code that stores a string: str = 'inet addr:127.0.0.1  Mask:255.0.0.0'.
+Use find and string slicing to extract the portion of the string
+after the colon inet address character and then use the
+rstrip function to remove any trailing characters.
+"""
+
+str = 'inet addr:127.0.0.1  Mask:255.0.0.0'
+
+at_por = str.find(':')
+portion = str[at_por+1:]
+print(portion.rstrip('  Mask:255.0.0.0'))
 
